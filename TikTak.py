85d6040d269d41e2e2fc9589b4e96707ab96748a
TukkerD <72821825+TukkerD@users.noreply.github.com>
2022-04-25 19:06:47 -0600

    trying to print the table

--- TikTak.py
+++ TikTak.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+# This is the game Tic-Tac-Toe created in python for CSE210.
+# Author : Tukker De Hart
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def table():
+    print(1 | 2 | 3
+          --+---+--
+          4 | 5 | 6
+          --+---+--
+          7 | 8 | 9)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "main":
+    table()
