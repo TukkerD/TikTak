9107c76b664610b97dfcbaf020a917c9ff29a203
TukkerD <72821825+TukkerD@users.noreply.github.com>
2022-04-25 19:09:50 -0600

    Printing the table

--- TikTak.py
+++ TikTak.py
@@ -2,12 +2,11 @@
 # Author : Tukker De Hart
 
 def table():
-    print(1 | 2 | 3
-          --+---+--
-          4 | 5 | 6
-          --+---+--
-          7 | 8 | 9)
+    print("1 | 2 | 3\n"
+          "--+---+--\n"
+          "4 | 5 | 6\n"
+          "--+---+--\n"
+          "7 | 8 | 9")
 
+table()
 
-if __name__ == "main":
-    table()
